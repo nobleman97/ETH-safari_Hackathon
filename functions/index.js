0e86a0ae1e77dcf1e44f7c14b017d27f073612b3
Loïc Darboux <loic.drbx@gmail.com>
2022-09-14 00:32:58 -0400

    add send sms feature

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
+require('dotenv').config()
+
 const functions = require("firebase-functions");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const twilio = require("twilio")(
+  process.env.TWILIO_ACCOUNT_SID,
+	process.env.TWILIO_AUTH_TOKEN
+);
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
+	// Print to cloud functions logger
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+	// Send sms to MY_PHONE_NUMBER using Twilio's client
+	twilio.messages
+		.create({
+			body: "Hello from Firebase! (sms)",
+			from: "+18146793741",
+			to: `${process.env.MY_PHONE_NUMBER}`
+		})
+		.then(message => console.log(message.sid))
+		.done();
+	// Send message in HTTP response body
+  response.send("Hello from Firebase! (API response)");
 });
