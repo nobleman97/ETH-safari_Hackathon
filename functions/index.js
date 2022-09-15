ba54b8830a13ef38e126b8e7b2f99a450bd50352
Loïc Darboux <loic.drbx@gmail.com>
2022-09-15 13:51:53 -0400

    implement send SMS in responce to account creation

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,31 @@
+// Loads environment variables from .env file
 require('dotenv').config()
 
+// Cloud Functions SDK to create Cloud Functions and set up triggers
 const functions = require("firebase-functions");
 
-const twilio = require("twilio")(
-  process.env.TWILIO_ACCOUNT_SID,
-	process.env.TWILIO_AUTH_TOKEN
-);
+// Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-const { MessagingResponse } = require('twilio').twiml;
+// Twilio helper library for creating and recieving SMS notifications
+const twilio = require("twilio")();
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  // Print to cloud functions logger
-  functions.logger.info("Hello logs!", {structuredData: true});
-	// Send sms to MY_PHONE_NUMBER using Twilio's client
-	twilio.messages
-		.create({
-			body: "Hello from Firebase! (sms)",
-			from: "+18146793741",
-			to: `${process.env.MY_PHONE_NUMBER}`
-		})
-		.then(message => console.log(message.sid))
-		.done();
-	// Send message in HTTP response body
-  response.send("Hello from Firebase! (API response)");
-});
+// Listens for new accounts added to /accounts/:documentId
+// and sends an onboarding SMS to the associated phone numbers
+exports.addAccount = functions.firestore.document("/accounts/{documentId}")
+	.onCreate((snap, context) => {
+		const { phoneNumber } = snap.data();
+		return twilio.messages
+			.create({
+				body: "Welcome to Safari Text! Reply with BALANCE, ALERTS, or TRIGGER to manage your CB Wallet account, or HELP for help.",
+				from: `${process.env.TWILIO_PHONE_NUMBER}`,
+				to: `${phoneNumber}`
+			})
+			.catch(error => console.log(error));
+	});
+
 
 exports.helloServer = functions.https.onRequest((request, response) => { 
   // When our twilio number recienves an inbound SMS
