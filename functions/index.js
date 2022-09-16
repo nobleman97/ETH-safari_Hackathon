b5056e358f6e73dda20e2444dc110a22de57b910
Loïc Darboux <loic.drbx@gmail.com>
2022-09-15 20:29:25 -0400

    implement SMS reception and reply webhook

--- functions/index.js
+++ functions/index.js
@@ -26,6 +26,30 @@ exports.addAccount = functions.firestore.document("/accounts/{documentId}")
 			.catch(error => console.log(error));
 	});
 
+// Webhook that recieves and replies to inbound SMS messages
+exports.inboundSMS = functions.https.onRequest(async (request, response) => {
+	const sender = request.body.From;
+	const address = await getAddress(sender);
+	if (address) {
+		const body = request.body.Body.toUpperCase();
+		const reply = new MessagingResponse();
+		switch (body) {
+			case 'BALANCE':
+				;
+				break;
+			case 'ALERTS':
+				;
+				break;
+			case 'TRIGGER':
+				reply.message(`This command will trigger a pre-signed transaction after asking you for confirmation. (Not yet implemented.)`);
+				break;
+			default:
+				reply.message(`That query is not supported... Reply with BALANCE, ALERTS, or TRIGGER to manage your CB Wallet account, or HELP for help.`);
+		}
+		return response.type("text/xml").send(reply.toString());
+	}
+	response.sendStatus(401); // Only registered users can use the service
+});
 
 exports.helloServer = functions.https.onRequest((request, response) => { 
   // When our twilio number recienves an inbound SMS
@@ -34,3 +58,17 @@ exports.helloServer = functions.https.onRequest((request, response) => {
   twiml.message("The Robots are coming! Head for the hills!");
   response.type("text/xml").send(twiml.toString());
 });
+
+// Get Ethereum addresss associated with a phone number
+// Returns undefined if sender doesn't have a Safari Text account
+async function getAddress(phoneNumber) {
+	const accountsRef = admin.firestore().collection("accounts"); 
+	const snapshot = await accountsRef.where("phoneNumber", "==", phoneNumber).get();
+	if (snapshot.empty) {
+		return undefined;
+	}
+	// TODO: Deal with case where a phone number is linked to multiple accounts
+	for (doc of snapshot.docs) {
+		return doc.data().address;
+	}
+}
